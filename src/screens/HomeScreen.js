f34bf2d492ecc25fee35d747d0a30860c45783fb
AnhducNA <ducank961@gmail.com>
2023-11-27 22:31:44 +0700

    3. HomeScreenUI: Bottom Tabs Navigator and Install FontAweSome

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,11 +1,85 @@
 import React from 'react';
 import {Text, View} from "react-native";
+import {createBottomTabNavigator} from "@react-navigation/bottom-tabs";
+import {FontAwesomeIcon} from "@fortawesome/react-native-fontawesome";
+import {
+    faHeart,
+    faMagnifyingGlass,
+    faMessage,
+    faUser,
+    faUserGroup
+} from "@fortawesome/free-solid-svg-icons";
+import ExploreScreen from "./ExploreScreen";
+import RoommateScreen from "./RoommateScreen";
+import WishlistsScreen from "./WishlistsScreen";
+import InboxScreen from "./InboxScreen";
+import ProfileScreen from "./ProfileScreen";
 
+const Tab = createBottomTabNavigator();
 const HomeScreen = () => {
     return (
-        <Text>
-            Home page
-        </Text>
+        <Tab.Navigator
+            screenOptions={({route}) => ({
+                tabBarActiveTintColor: '#0b6839',
+                tabBarInactiveTintColor: 'gray',
+                headerShown: false,
+                tabBarIcon: ({focused, color, size}) => {
+                    let colorName = "gray";
+                    let opacity = 0.9;
+                    let iconSize = 25;
+                    if (focused) {
+                        colorName = "#0b6839";
+                    }
+                    if (route.name === "Explore") {
+                        return (
+                            <FontAwesomeIcon icon={faMagnifyingGlass}
+                                             color={colorName}
+                                             opacity={opacity}
+                                             size={iconSize}
+                            />
+                        )
+                    } else if (route.name === "Roommate") {
+                        return (
+                            <FontAwesomeIcon icon={faUserGroup}
+                                             color={colorName}
+                                             opacity={opacity}
+                                             size={iconSize}
+                            />
+                        )
+                    } else if (route.name === "Wishlists") {
+                        return (
+                            <FontAwesomeIcon icon={faHeart}
+                                             color={colorName}
+                                             opacity={opacity}
+                                             size={iconSize}
+                            />
+                        )
+                    } else if (route.name === "Inbox") {
+                        return (
+                            <FontAwesomeIcon icon={faMessage}
+                                             color={colorName}
+                                             opacity={opacity}
+                                             size={iconSize}
+                            />
+                        )
+                    } else if (route.name === "Profile") {
+                        return (
+                            <FontAwesomeIcon icon={faUser}
+                                             color={colorName}
+                                             opacity={opacity}
+                                             size={iconSize}
+                            />
+                        )
+                    }
+                },
+            })}
+        >
+            <Tab.Screen name="Explore" component={ExploreScreen}/>
+            <Tab.Screen name="Roommate" component={RoommateScreen}/>
+            <Tab.Screen name="Wishlists" component={WishlistsScreen}/>
+            <Tab.Screen name="Inbox" component={InboxScreen}/>
+            <Tab.Screen name="Profile" component={ProfileScreen}/>
+        </Tab.Navigator>
     );
 };
 
---
+++ src/screens/InboxScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from "react-native";
+
+const InboxScreen = () => {
+    return (
+        <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+            <Text>Inbox screen!</Text>
+        </View>
+    );
+};
+
+export default InboxScreen;
